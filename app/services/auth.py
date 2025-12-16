ba010a4095b5897f17ebbe9dbe8f4bc1f9322ba3
wexifol <thevovanplay@gmail.com>
2025-12-16 19:52:37 +0300

    Create auth.py
    add auth service

---
+++ app/services/auth.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+import jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = "SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
